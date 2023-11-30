624021da0b3012a73fae3567d6300992ce88a4f6
ronload <91997734+ronload@users.noreply.github.com>
2023-11-30 23:20:27 +0800

    Connect HTML and Flask.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 
 app = Flask(
@@ -9,7 +9,23 @@ app = Flask(
 
 @app.route("/")
 def index():
-    return "Welcome to home page."
+    return render_template("index.html")
+
+@app.route("/search_CustomerInfo")
+def search_customer_info():
+    return render_template("search_CustomerInfo.html")
+
+@app.route("/search_ProductInfo")
+def search_product_info():
+    return render_template("search_ProductInfo.html")
+
+@app.route("/search_OrderInfo")
+def search_order_info():
+    return render_template("search_OrderInfo.html")
+
+@app.route("/search_CartInfo")
+def search_cart_info():
+    return render_template("search_CartInfo.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
